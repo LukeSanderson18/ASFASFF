be7cf09edb92f9ede346656d754f45a656c12c93
Pablo Armando Rodríguez Codes <parmandorc@gmail.com>
2017-01-20 20:17:05 +0100

    Added basic player movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+    public float movementSpeed = 1.0f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +12,15 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        float dx = Input.GetAxis("Horizontal");
+        if (dx != 0.0f)
+        {
+            gameObject.transform.Translate(dx * movementSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            gameObject.GetComponent<Animator>().Play("JumpIn");
+        }
 	}
 }
