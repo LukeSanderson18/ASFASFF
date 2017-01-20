ea5a483e79eebbb14db652b7a336496629fb2d5e
Luke Sanderson <cactuar18@hotmail.com>
2017-01-20 20:20:05 +0000

    Instrument code staerted
    adsohjofhsofhadfh

--- Assets/Instrument.cs
+++ Assets/Instrument.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class Instrument : MonoBehaviour {
 
+    public float health;
+    float randDetoriation;
 	// Use this for initialization
 	void Start () {
-	
+
+        randDetoriation = Random.Range(2f, 10f);
 	}
 	
 	// Update is called once per frame
