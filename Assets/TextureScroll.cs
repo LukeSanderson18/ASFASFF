aa600ed79a1e1da07d03ca77c61da22425ddc928
Pablo Armando Rodríguez Codes <parmandorc@gmail.com>
2017-01-20 21:27:53 +0100

    Added scroll

---
+++ Assets/TextureScroll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureScroll : MonoBehaviour {
+
+    public float scrollSpeed = 1.0f;
+    private float scrollPosition = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        scrollPosition += scrollSpeed * Time.deltaTime;
+        gameObject.GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(scrollPosition, 0));
+	}
+}
