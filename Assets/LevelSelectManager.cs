fd4d13aa8f9439dc04a547af6e38c4aa920f5a38
Luke Sanderson <cactuar18@hotmail.com>
2017-01-21 15:10:34 +0000

    fixed clicking button behind curtains
    ohhhhhhhh yeaaaaaaaaaaa

--- Assets/LevelSelectManager.cs
+++ Assets/LevelSelectManager.cs
@@ -17,17 +17,39 @@ public class LevelSelectManager : MonoBehaviour {
     public GameObject Trumpet;
     public GameObject Violin;
     public GameObject Tuba;
+    float clicked = -1;
 
     public void OnClick()
     {
-        print("clicked!");
+        if (GameObject.Find("Curtains").GetComponent<CurtainManager>().open)
+        {
+            print("clicked!");
 
-        Piano.GetComponent<Instrument>().Init(goodPiano, badPiano, false);
-        Trumpet.GetComponent<Instrument>().Init(goodTrumpet, badTrumpet, false);
-        Violin.GetComponent<Instrument>().Init(goodViolin, badViolin, false);
-        Tuba.GetComponent<Instrument>().Init(goodTuba, badTuba, false);
+            Piano.GetComponent<Instrument>().Init(goodPiano, badPiano, false);
+            Trumpet.GetComponent<Instrument>().Init(goodTrumpet, badTrumpet, false);
+            Violin.GetComponent<Instrument>().Init(goodViolin, badViolin, false);
+            Tuba.GetComponent<Instrument>().Init(goodTuba, badTuba, false);
 
-        GameState.Paused = false;
+            clicked = 0;
+            GameState.Paused = false;
+        }
+
+
+    }
+
+    void Update()
+    {
+        if (clicked>=0)
+        {
+            clicked += Time.deltaTime;
+        }
+        if(clicked > 1)
+        {
+            transform.parent.position = new Vector3
+            (transform.parent.position.x,
+            Mathf.Lerp(transform.parent.position.y, 20, Time.deltaTime * 3),
+            transform.parent.position.z);
+        }
     }
 
 }
