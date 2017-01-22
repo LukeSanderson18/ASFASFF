0d2e805ed8335f35b91c734124cd1fbc7be62e86
Luke Sanderson <cactuar18@hotmail.com>
2017-01-22 11:21:30 +0000

    Last song, fixes, crowd noise

---
+++ Assets/crowdTalk.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crowdTalk : MonoBehaviour {
+
+	AudioSource aud;
+
+    void Start()
+    {
+        aud = GetComponent<AudioSource>();
+    }
+	// Update is called once per frame
+	void Update () {
+
+        if (GameState.Paused)
+        {
+            aud.volume = Mathf.Lerp(aud.volume, 0.8f, Time.deltaTime);
+        }
+        else
+        {
+            aud.volume = Mathf.Lerp(aud.volume, 0, Time.deltaTime);
+        }
+
+		
+	}
+}
